7eb79b4ac8c6f6544ae5a430b6a29ef1bb3aca1b
David Frödin <dawid.frodin@gmail.com>
2021-02-05 09:54:32 +0100

    Add project files.

---
+++ Inlämning1Sql/DatabaseSql.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Text;
+
+namespace Inlämning1Sql
+{
+    class DatabaseSql
+    {
+        private string ConnectionString { get; set; } = @"Data Source=.\SQLExpress;Integrated Security=true;database={0}";
+        private string DatabaseName { get; set; } = "";
+
+        public DataTable GetDataTable(string sqlString, params(string,string) [] parameters)
+        {
+            var dt = new DataTable();
+            var connString = string.Format(ConnectionString, DatabaseName);
+
+            using (var cnn = new SqlConnection(connString))
+            {
+                cnn.Open();
+                using(var command = new SqlCommand(sqlString,cnn))
+                {
+                    foreach(var item in parameters)
+                    {
+                        command.Parameters.AddWithValue(item.Item1, item.Item2);
+                    }
+
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dt);
+                    }
+
+                }
+
+            }
+            return dt;
+        }
+
+        private static void SetParameters((string, string)[] parameters, SqlCommand command)
+        {
+            foreach (var item in parameters)
+            {
+                command.Parameters.AddWithValue(item.Item1, item.Item2);
+            }
+        }
+
+    }
+}
---
+++ Inlämning1Sql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämning1Sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
